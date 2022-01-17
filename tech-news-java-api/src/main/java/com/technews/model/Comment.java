bea0d0365ccc7af36de143c78e049d11ace41a13
ntjohns1 <nelsontjohns@gmail.com>
2022-01-17 16:22:34 -0500

    Created controller classes

--- tech-news-java-api/src/main/java/com/technews/model/Comment.java
+++ tech-news-java-api/src/main/java/com/technews/model/Comment.java
@@ -3,6 +3,7 @@ package com.technews.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import javax.persistence.*;
+//import java.io.Serializable;
 import java.io.Serializable;
 import java.util.Objects;
 
